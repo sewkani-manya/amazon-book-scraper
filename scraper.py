348473b750809ca2e99195782167c8d18a598d86
sewkani-manya <sewkani.manya564@gmail.com>
2021-04-08 14:50:58 +0530

    extracting the HTML page of the declared URL

--- scraper.py
+++ scraper.py
@@ -1,3 +1,11 @@
 # scraper.py
 
-print("Hello World")
+#import libraries
+import requests
+from bs4 import BeautifulSoup
+
+#specify the url
+bestseller_page ="https://www.amazon.in/gp/bestsellers/books/ref=zg_bs_nav_0"
+
+#query the website and return the html of the variable
+response=requests.get(bestseller_page)
