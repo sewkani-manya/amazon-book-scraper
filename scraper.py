62926f353f808dd04dce8b8c7c3283164c473b67
sewkani-manya <sewkani.manya564@gmail.com>
2021-04-08 14:54:53 +0530

    Parse the page into BeautifulSoup format

--- scraper.py
+++ scraper.py
@@ -9,3 +9,9 @@ bestseller_page ="https://www.amazon.in/gp/bestsellers/books/ref=zg_bs_nav_0"
 
 #query the website and return the html of the variable
 response=requests.get(bestseller_page)
+#parse the HTML using beautiful soup and storing in a variable soup
+soup=BeautifulSoup(response.content, "html.parser")
+print(soup.encode("utf-8"))
+
+# Take out the <div> of the name and get its value
+book_name_div= soup.find("div", attrs={'class':'p13n-sc-truncate'})
