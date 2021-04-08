f22083561554e606b1fe2f8b1724db4a9a40a104
sewkani-manya <sewkani.manya564@gmail.com>
2021-04-08 15:50:32 +0530

    Code cleanup

--- scraper.py
+++ scraper.py
@@ -1,25 +1,23 @@
 # scraper.py
+# TODO:
+# - Update readme with all details about how you went through this project
 
 #import libraries
 import requests
 from bs4 import BeautifulSoup
 
 #specify the url
-bestseller_page ="https://www.amazon.in/gp/bestsellers/books/ref=zg_bs_nav_0"
+bestseller_url = "https://www.amazon.in/gp/bestsellers/books/ref=zg_bs_nav_0"
 
 #query the website and return the html of the variable
-response=requests.get(bestseller_page)
+response = requests.get(bestseller_url)
 
 #parse the HTML using beautiful soup and storing in a variable soup
-soup=BeautifulSoup(response.content, "html.parser")
-
+page = BeautifulSoup(response.content, "html.parser")
 
 # Take out the <div> of the name and get its value
-book_name_divs= soup.find_all("div", attrs={'class':'p13n-sc-truncate'})
+book_name_divs = page.find_all("div", attrs={'class':'p13n-sc-truncate'})
 
-for book_name in book_name_divs:
-    print(book_name.get_text())
-
-
-# TODO:
-# - Update readme with all details about how you went through this project
+print("Amazon Bestseller's List:")
+for b in book_name_divs:
+    print(b.get_text())
